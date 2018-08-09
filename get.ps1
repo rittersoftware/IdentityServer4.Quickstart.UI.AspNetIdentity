99d3e330f5d1b119ab824b1058213af4a117a1b1
Brock Allen <brockallen@gmail.com>
2018-08-09 12:23:00 -0400

    Update get.ps1

--- get.ps1
+++ get.ps1
@@ -1,4 +1,4 @@
-$source = "https://github.com/IdentityServer/IdentityServer4.Quickstart.UI.AspNetIdentity/archive/dev.zip"
+$source = "https://github.com/IdentityServer/IdentityServer4.Quickstart.UI.AspNetIdentity/archive/release.zip"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest $source -OutFile ui.zip
 
@@ -8,9 +8,9 @@ if (!(Test-Path -Path Quickstart))  { mkdir Quickstart }
 if (!(Test-Path -Path Views))       { mkdir Views }
 if (!(Test-Path -Path wwwroot))     { mkdir wwwroot }
 
-copy .\ui\IdentityServer4.Quickstart.UI.AspNetIdentity-dev\Quickstart\* Quickstart -recurse -force
-copy .\ui\IdentityServer4.Quickstart.UI.AspNetIdentity-dev\Views\* Views -recurse -force
-copy .\ui\IdentityServer4.Quickstart.UI.AspNetIdentity-dev\wwwroot\* wwwroot -recurse -force
+copy .\ui\IdentityServer4.Quickstart.UI.AspNetIdentity-release\Quickstart\* Quickstart -recurse -force
+copy .\ui\IdentityServer4.Quickstart.UI.AspNetIdentity-release\Views\* Views -recurse -force
+copy .\ui\IdentityServer4.Quickstart.UI.AspNetIdentity-release\wwwroot\* wwwroot -recurse -force
 
 del ui.zip
 del ui -recurse
